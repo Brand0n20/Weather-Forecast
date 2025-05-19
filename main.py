ecc0b902f2726d7741d23a603bdb8ffe5f23f27a
Brandon Alfaro <brandonalfaro20@gmail.com>
2025-05-18 21:49:03 -0500

    added main file and imported packages

---
+++ main.py
@@ -0,0 +1,13 @@
+# Weather App using external API
+
+# importing the libraries
+from tkinter import *
+import requests
+import json
+import datetime
+from PIL import ImageTk, Image
+
+root = Tk()
+root.title("Weather App")
+root.geometry("450x700")
+root['background'] = "white"
